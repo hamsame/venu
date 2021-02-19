5bf8d7984bd4f84063c96eeb5dd7a4879624ac41
Hamsa Elmi <73555504+hamsame@users.noreply.github.com>
2021-02-19 21:21:25 +0000

    Update: Added maps, changed animations
    Added map with Mapbox API, changed animations, made the images conform to various screen sizes

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-// // Regex for contact form
-// document.getElementById("name").addEventListener("blur", nameCheck);
-// document.getElementById("message").addEventListener("blur", messageCheck);
-// document.getElementById("submit-btn").addEventListener("click", submit);
+// form validation
+const firstName = document.getElementById("fname");
+const lastName = document.getElementById("lname");
+const email = document.getElementById("email");
+const textArea = document.getElementById("message");
+const submitBtn = document.getElementById("submit");
+const form = document.getElementById("form");
 
-// function nameCheck() {
-//   const usersName = document.getElementById("name");
-//   let re = /^[A-z]{2,10}\s[A-z]{2,10}$/;
-//   const errorMsg = document.createElement("p");
-//   errorMsg.textContent = "Please add your name";
-//   const inputs = document.querySelector(".contact-form");
+form.addEventListener("submit", (e) => {
+  let messages = [];
+  if (firstName.value == null || firstName.value == "") {
+    messages.push("Enter your first name");
+  }
+  if (lastName.value == null || lastName.value == "") {
+    messages.push("Enter your last name");
+  }
+  if (email.value == null || email.value == "") {
+    messages.push("Enter your email address");
+  }
+  if (textArea.value == null || textArea.value == "") {
+    messages.push("Enter a message");
+  }
 
-//   !re.test(usersName.value)
-//     ? inputs.insertBefore(errorMsg, document.getElementById("email"))
-//     : "";
-// }
-
-// function messageCheck() {
-//   const message = document.getElementById("message").value;
-//   let re = /^[A-z0-9]/;
-
-//   !re.test(message) ? console.log("incorrect") : console.log("correct");
-// }
-
-// function submit() {
-//   nameCheck();
-//   messageCheck();
-// }
+  if (messages.length > 0) {
+    e.preventDefault();
+    document.getElementById("error").innerText = messages.join(", ");
+  } else {
+    alert("Form Submitted");
+  }
+});
